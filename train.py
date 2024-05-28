56c6089800e9cf08d6ab1a5ed41bc6db9dc164a6
Akito-UzukiP <12011903@mail.sustech.edu.cn>
2024-05-29 02:03:54 +0800

    train.py

--- train.py
+++ train.py
@@ -23,24 +23,24 @@ env = make_vec_env(GakuenIdolMasterEnv, n_envs=40)
 #     n_steps=2048,  # 设置n_steps
 #     batch_size=8192  # 调整批量大小，使其成为 n_steps * n_envs 的因数
 # )
-# model = DQN(
-#     "MultiInputPolicy",
-#     env,
-#     verbose=1,
-#     tensorboard_log="./log",
-#     device=get_device(),
-#     policy_kwargs=policy_kwargs,
-#     batch_size=1024,  # 调整批量大小，使其成为 n_steps * n_envs 的因数
-#     exploration_final_eps=0.05,
-# )
+model = DQN(
+    "MultiInputPolicy",
+    env,
+    verbose=1,
+    tensorboard_log="./log",
+    device=get_device(),
+    policy_kwargs=policy_kwargs,
+    batch_size=1024,  # 调整批量大小，使其成为 n_steps * n_envs 的因数
+    exploration_final_eps=0.05,
+)
 
-model = DQN.load("dqn_gakuen_idol_master", device=get_device(), env=env, force_reset=True)
-model.exploration_fraction = 0
-model.exploration_initial_eps = 0.05
-model.exploration_final_eps = 0.05
-model.exploration_final_eps = 0.05
-model.learning_starts = 0
-model.start_time = 0
+# model = DQN.load("dqn_gakuen_idol_master", device=get_device(), env=env, force_reset=True)
+# model.exploration_fraction = 0
+# model.exploration_initial_eps = 0.05
+# model.exploration_final_eps = 0.05
+# model.exploration_final_eps = 0.05
+# model.learning_starts = 0
+# model.start_time = 0
 
 
 # 设置保存检查点的回调函数，每保存10000步
@@ -48,7 +48,7 @@ model.start_time = 0
 #                                          name_prefix='dqn_gakuen_idol_master')
 
 # 开始训练并使用回调函数保存检查点
-for i in range(1):
+if __name__ == "__main__":
     model.learn(total_timesteps=4096000, log_interval=1, reset_num_timesteps=False)
 
     # 手动保存模型（可选）
